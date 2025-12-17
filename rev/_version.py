6d3ab440fcb23b584c793f4c40c412742e8739da
YEP <redsand@redsand.net>
2025-12-17 15:07:32 -0600

    Rev now starting to work as expected

--- rev/_version.py
+++ rev/_version.py
@@ -3,6 +3,6 @@
 # Note: REV_GIT_COMMIT should be populated at build time so wheels/sdists carry
 # the commit even when git metadata is unavailable at runtime.
 REV_VERSION = "2.0.1"
-REV_GIT_COMMIT = "dc9aeded73642ee4cb229b96fb4f163b0346bd3e"
+REV_GIT_COMMIT = "9154443725f73941fb92b70f1174f7180da89d29"
 
 __all__ = ["REV_VERSION", "REV_GIT_COMMIT"]
