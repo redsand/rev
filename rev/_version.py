1c2f02ab1945464c8ce133bdd471a7bbc6a7b842
YEP <redsand@redsand.net>
2025-12-17 21:47:58 -0600

    tightened up executor

--- rev/_version.py
+++ rev/_version.py
@@ -3,6 +3,6 @@
 # Note: REV_GIT_COMMIT should be populated at build time so wheels/sdists carry
 # the commit even when git metadata is unavailable at runtime.
 REV_VERSION = "2.0.1"
-REV_GIT_COMMIT = "941581c3e17b674d8b7860fcfbc1c90130ba3fc2"
+REV_GIT_COMMIT = "1263151e99955726a2a0fb4e071cdc5ef813a146"
 
 __all__ = ["REV_VERSION", "REV_GIT_COMMIT"]
