88e91c21f9bad024326a11282db69ae2763e7d57
YEP <redsand@redsand.net>
2025-12-18 15:32:53 -0600

    adding tui

--- rev/_version.py
+++ rev/_version.py
@@ -3,6 +3,6 @@
 # Note: REV_GIT_COMMIT should be populated at build time so wheels/sdists carry
 # the commit even when git metadata is unavailable at runtime.
 REV_VERSION = "2.0.1"
-REV_GIT_COMMIT = "427ee6fef2cbafc01c7dc9c3aa7078eb53b8fb20"
+REV_GIT_COMMIT = "e8a429bcb9fd1e37c0ae627b193646ad2f68d9d8"
 
 __all__ = ["REV_VERSION", "REV_GIT_COMMIT"]
