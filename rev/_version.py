8ac14c04a349864d19192b1be979a30e5e0f00d2
YEP <redsand@redsand.net>
2025-12-22 21:37:29 -0600

    fixing err

--- rev/_version.py
+++ rev/_version.py
@@ -3,6 +3,6 @@
 # Note: REV_GIT_COMMIT should be populated at build time so wheels/sdists carry
 # the commit even when git metadata is unavailable at runtime.
 REV_VERSION = "2.1.0"
-REV_GIT_COMMIT = "4e1849d8966b5c7a6e9e3267dcb2801a16f370f1"
+REV_GIT_COMMIT = "eb111522a39b15ceae635a9c6c6d32f4c665a2fa"
 
 __all__ = ["REV_VERSION", "REV_GIT_COMMIT"]
