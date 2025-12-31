4080166f3e1d17f25890e0ddc2f254becec4b3fd
YEP <redsand@redsand.net>
2025-12-30 18:51:52 -0600

    close

--- rev/_version.py
+++ rev/_version.py
@@ -3,6 +3,6 @@
 # Note: REV_GIT_COMMIT should be populated at build time so wheels/sdists carry
 # the commit even when git metadata is unavailable at runtime.
 REV_VERSION = "2.1.1"
-REV_GIT_COMMIT = "3802d0d507320dcec6e4812ec39325ef7ef3f7eb"
+REV_GIT_COMMIT = "f384f756fec139b7e2c88b5201ad0a114eb82e45"
 
 __all__ = ["REV_VERSION", "REV_GIT_COMMIT"]
