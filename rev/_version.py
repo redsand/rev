4dd0d35706543d7dbb034a320c786afd674ed541
YEP <redsand@redsand.net>
2026-01-04 23:05:35 -0600

    fixing pathcerr

--- rev/_version.py
+++ rev/_version.py
@@ -3,6 +3,6 @@
 # Note: REV_GIT_COMMIT should be populated at build time so wheels/sdists carry
 # the commit even when git metadata is unavailable at runtime.
 REV_VERSION = "2.1.1"
-REV_GIT_COMMIT = "5e5558fc57ad748d3c5bc5cec0e6219328bb3711"
+REV_GIT_COMMIT = "db4ebd00f4e32fe9c1463df8051f6a0e075814db"
 
 __all__ = ["REV_VERSION", "REV_GIT_COMMIT"]
